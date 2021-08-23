fb2229e2eabe0ea7fa4ef539f99c3ef3c81936cc
Yashasvi Sinha <yashasvi.sinha@attainu.com>
2021-08-23 23:28:03 +0530

    added intro to js

---
+++ week-21/day-1/js/myScript.js
@@ -0,0 +1,72 @@
+// var myVariable = 8254215;
+// myVariable = "Yash";
+
+
+// var i = 13, j ="asd", c = true, z
+
+// function sum(n1, n2) {
+    
+//     return 120398;
+// }
+
+// sum(502, 443)
+
+var str = "ABC" //global
+var num = 100;
+
+var zx = 123;
+
+
+
+function xyz (){
+    var num = 333;
+    console.log(str) //
+    console.log(num) //
+
+}
+
+function abc(){
+    // var num = 123;
+
+    console.log(num) // 100
+
+} 
+
+
+
+// abc();
+// xyz()
+
+
+// Data Types
+
+// 9 Types ==
+
+var abc = "acsb" + "uyjh" //acsbuyjh
+var zxc = 'bob' + " the builder"; // bop the builder
+var zxc = 10 / 3
+
+
+/*
+    1. Primitive
+
+        a. Number // 123, 44,55.2 -74  (96135498432165)
+        b. String // '', ""  (Immuatble)
+        c. Boolean // true, false, (1, 0)
+        d. BigInt // 123094810293480219384019238401923840192384
+        e. Undefined // var ax = undefined // var ax = null
+        f. Symbol 
+
+    2. Structural
+
+        a. Object
+        b. Function
+
+
+    3. Structural Root Primitive
+        null 
+
+
+
+*/
+
