650c3098abb432995f65746c3803596496d5a22f
Yashasvi Sinha <yashasvi.sinha@attainu.com>
2021-08-24 23:06:08 +0530

    added if and loops

---
+++ week-21/day-2/js/myScript.js
@@ -0,0 +1,118 @@
+
+
+// if
+
+// if abc is 123
+
+var abc = "123" 
+
+// "123" is string
+// 123 is a number
+
+// if (abc === 123){ //type casting
+
+//     // then do this
+//     console.log("Inside the if block")
+
+// }else if (abc == "123"){
+    
+//     console.log("Inside the if-else block")
+
+// }else{
+//     console.log("Else Block")
+// }
+
+// if (abc === 1 && abc == 10){
+
+// }
+
+// console.log("Outside")
+
+// elif  ===> else if
+// else  ===> else
+
+// var isAbcIsGrater = abc === 123 && (bca <= 55 || cda <= 123)
+// if (isAbcIsGrater) {
+    // 
+// }
+
+// var myVariable = 999
+
+// switch (myVariable) {
+    
+//     case 4 :
+//         console.log("The value is 4")
+//         break
+
+//     case 100:
+//         console.log("The value is 100")
+//         break
+
+//     default:
+//         console.log("Default");
+//         break
+// }
+
+// console.log('outside')
+
+// if (myVariable === 4){
+//     console.log("The value is 4")
+// }
+
+
+
+
+//In Python == for, while
+
+// var count = 0
+
+// while(count <= 10){
+//     // console.log(count)
+//     count += 1
+
+// }
+
+
+// for( var count=0; count <= 10; count++ ){
+
+//     console.log(count)
+// }
+
+// console.log(count)
+
+
+// var count = 0
+// do{
+
+//     console.log(count)
+//     count++
+
+// }while(count <= 10 )
+
+
+
+
+
+var x = 1
+
+while(x > 10){
+    console.log("In while loop")
+    x--
+}
+console.log("Outside")
+
+
+var z = 1
+do{
+    console.log("In do while loop")
+    z--
+}while(z > 10)
+
+
+// var result = "251"; //typecast
+
+25+1+""+1+2-3 
+"261" + 2
+"2612" - 2
+
+2612 - 3
