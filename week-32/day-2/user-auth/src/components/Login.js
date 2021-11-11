33119d4ba58bc5013e75da7aa688052e86187aaa
Yashasvi Sinha <yashasvi.sinha@attainu.com>
2021-11-11 22:53:36 +0530

    added validations using Joi

--- week-32/day-2/user-auth/src/components/Login.js
+++ week-32/day-2/user-auth/src/components/Login.js
@@ -2,29 +2,56 @@ import { useState } from "react"
 import { userLogin } from "../actions"
 import { useDispatch, useSelector } from 'react-redux'
 import { Navigate } from 'react-router-dom'
+import Joi from 'joi';
 
 function Login() {
-
+    
     const userStore = useSelector(state => state.user)
     const dispatch = useDispatch()
     
+    console.log(`🚀 ~ Joi`, Joi)
     console.log(`🚀 ~ Login ~ userStore`, userStore)
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
+    const [validationErr, setValidationErr] = useState({})
+
+
+    const userCredSchema = Joi.object({
+        email: Joi.string().required().min(4).label("E-Mail"),
+        pass: Joi.string().required().min(6)
+    })
+
+
+
 
     if (userStore.isLoggedIn === true) {
         return <Navigate to="/restaurants"/>
     }
     
     const handleLoginClick = () => {
-        dispatch(userLogin(email, password))
+
+        //validation
+        
+        
+        const result = userCredSchema.validate({ email: email, pass: password })
+
+        console.log(`🚀 ~ handleLoginClick ~ result`, result)
+        
+        if (result.error) {
+            setValidationErr(result.error)
+        }else{
+            dispatch(userLogin(email, password))
+        }
+        
     }
+
 
     return (
         <div>
             <input type="email" onChange={ (e) => setEmail(e.target.value) } value={email} />
             <input type="password" onChange={ (e) => setPassword(e.target.value) } value={password} />
             <button onClick={handleLoginClick}>Login</button>
+            {validationErr.message && <span>{validationErr.message}</span>}
         </div>
     )
 }
