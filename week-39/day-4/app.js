5160632be8261891cac5d3da2cfee4b9e7c106be
Yashasvi Sinha <yashasvi.sinha@attainu.com>
2021-12-30 22:55:39 +0530

    added graphql lecture

--- week-39/day-4/app.js
+++ week-39/day-4/app.js
@@ -1,35 +1,69 @@
 const express = require('express');
+const { buildSchema } = require('graphql')
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
 
-// Construct a schema, using GraphQL schema language
-const allSchema = buildSchema(`
-  type Query {
-    userName: String,
-    email: String
-    age: Int,
-    getLowerString(strToLower: String): String
-  }
-`);
-
-// The root provides a resolver function for each API endpoint
-const root = {
-  userName: () => {
-    return 'WierdStuff';
-  },
-  email: () => 'yash@a.com',
-  age: () => 43,
-  getLowerString: (args) => {
-
-    return args.strToLower.toLowerCase()
-  }
-};
+const bookDb = [
+    {
+        "abcser132": {
+            bookName: "Harry Potter",
+            price: 125.5
+        },
+        "vnkasd4431": {
+            bookName: "For the love of Physics",
+            price: 875.55
+        }
+    }
+]
 
 const app = express();
-app.use('/graphql', graphqlHTTP({
-  schema: allSchema,
-  rootValue: root,
-  graphiql: true,
-}));
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/graphql');
+
+// Construct the schema that GraphQL will use/understand
+// using the GraphQL Schema definition language
+const mySchema = buildSchema(`
+    type Query{
+        userName: String,
+        email: String,
+        age: Int,
+        price: Float,
+        convertToLower(strToLower: String!, removeSpace: Boolean): String
+    }
+`)
+
+// Define resolver functions for corresponding query fields to obtain the values for that field
+const rootResolvers = {
+    convertToLower: (argv) => {
+        console.log(`🚀 ~ argv`, argv)
+        let convertLowerString = argv.strToLower.toLowerCase()
+        console.log(`🚀 ~ convertLowerString`, convertLowerString)
+        
+        if (argv.removeSpace === true) {
+            convertLowerString = convertLowerString.replaceAll(" ", "")
+            console.log(`🚀 ~ Remove Space convertLowerString`, convertLowerString)
+        }
+
+        return convertLowerString
+    },
+    userName: () => {
+        
+        return "pasta123"
+    },
+    email: () => {
+        return "abc@pqr.com"
+    },
+    age: () => {
+        return 55
+    },
+    price: () => 79.66
+}
+
+
+app.use('/graphql',  graphqlHTTP({
+    schema: mySchema,
+    rootValue: rootResolvers,
+    graphiql: true
+}) )
+
+
+app.listen(5000, () => {
+    console.log("Listening")
+});
